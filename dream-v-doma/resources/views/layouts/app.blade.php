4391f8a1ce9711e68d1a1e19e18d77d55e47b89f
Mykhailo <ignatyuk71@gmail.com>
2025-08-24 21:44:39 +0200

    build assets for production

--- dream-v-doma/resources/views/layouts/app.blade.php
+++ dream-v-doma/resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
   <link rel="preload" href="/assets/css/theme.min.css" as="style">
   <link rel="stylesheet" href="/assets/css/theme.min.css" id="theme-styles">
 
-  @vite(['resources/css/app-index.css', 'resources/js/app.js'])
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
   @stack('styles')
 </head>
 <body>
