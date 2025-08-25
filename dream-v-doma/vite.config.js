fb64e13df8ba21d9d219d5a79c1bc6ce32884959
Mykhailo <ignatyuk71@gmail.com>
2025-08-25 22:41:28 +0200

    build assets for production

--- dream-v-doma/vite.config.js
+++ dream-v-doma/vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
         'resources/js/app.js',               // фронт
         'resources/js/admin/index.js',       // ✅ адмінка js
         'resources/js/admin/index.css',       // ✅ адмінка css (в одному input)
-        'resources/css/app-index.css'
+        'resources/css/app-index.css',
+        'resources/css/app.css',
       ],
       refresh: true,
     }),
