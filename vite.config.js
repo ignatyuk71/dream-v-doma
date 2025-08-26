cd5479f07f859aa1c1193f8b2a63e397508b9b4b
Mykhailo <ignatyuk71@gmail.com>
2025-08-26 21:55:46 +0200

    build: include resources/css/app-index.css in Vite maniпрарпм орр

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
         'resources/js/admin/index.js',       // ✅ адмінка js
         'resources/js/admin/index.css',       // ✅ адмінка css (в одному input)
         'resources/css/app-index.css',
-        'resources/css/app.css',
       ],
       refresh: true,
     }),
