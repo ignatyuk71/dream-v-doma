9b76761d84108a4c2721dd5a17b6416a02bf313d
Mykhailo <ignatyuk71@gmail.com>
2025-09-21 10:00:21 +0200

    build: add public/build for shared 1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,22 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+/**
+ * Визначаємо, який .env завантажувати.
+ * - Для dream-v-doma.com.ua -> .env.comua
+ * - Для всіх інших (у т.ч. .site) -> .env.site
+ * - Можна примусово задати APP_ENV_FILE у вебсервері/CLI
+ */
+$host = strtolower($_SERVER['HTTP_HOST'] ?? '');
+$envFile = $_SERVER['APP_ENV_FILE'] ?? (
+    str_contains($host, 'dream-v-doma.com.ua') ? '.env.comua' : '.env.site'
+);
+
+return Application::configure(
+        basePath: dirname(__DIR__)
+    )
+    // ВАЖЛИВО: цей рядок каже Laravel, який .env використати
+    ->loadEnvironmentFrom($envFile)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,4 +30,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
