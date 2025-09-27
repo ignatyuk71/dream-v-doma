571aa4b622defe02115240fbb3d6ae82f0ee0374
Mykhailo <ignatyuk71@gmail.com>
2025-09-27 12:18:42 +0200

    build: add public/build for shared API Conversia 1

--- resources/views/partials/meta-pixel-noscript.blade.php
+++ resources/views/partials/meta-pixel-noscript.blade.php
@@ -1,15 +1,23 @@
 @php
   $t = \Illuminate\Support\Facades\DB::table('tracking_settings')->first();
-  $pixelId  = $t->pixel_id ?? null;
-  $enabled  = $t
-              && (int)($t->pixel_enabled ?? 0) === 1
-              && !empty($pixelId)
-              && !((int)($t->exclude_admin ?? 1) === 1 && request()->is('admin*'));
+
+  // безпечне зчитування, навіть якщо $t === null
+  $pixelId = $t?->pixel_id ?? null;
+
+  $enabled = $t
+    && (int)($t->pixel_enabled ?? 0) === 1
+    && !empty($pixelId)
+    && !((int)($t->exclude_admin ?? 1) === 1 && request()->is('admin*'));
 @endphp
 
-@if ($enabled)
-<noscript>
-  <img height="1" width="1" style="display:none"
-       src="https://www.facebook.com/tr?id={{ $pixelId }}&ev=PageView&noscript=1" />
-</noscript>
+@if ($enabled && $pixelId)
+  <noscript>
+    <img
+      src="https://www.facebook.com/tr?id={{ $pixelId }}&ev=PageView&noscript=1"
+      height="1"
+      width="1"
+      style="display:none"
+      alt=""
+    />
+  </noscript>
 @endif
